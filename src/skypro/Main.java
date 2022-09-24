2adc5864038f4cd70f3de244595323e83b48268c
rudskaya irina <irarudskaya5@gmail.com>
2022-09-24 14:38:18 +0300

    Task 4

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -5,16 +5,16 @@ public class Main {
 
         byte bottleTwoMinutes = 16;
         int bottleTwentyMinutes = bottleTwoMinutes * 10;
-        System.out.println("За 20 минут машина произвела " + bottleTwentyMinutes + " штук бутылок");
+        System.out.println("За 20 минут машина произвела " + bottleTwentyMinutes + " бутылок");
 
         int bottleOneDay = bottleTwoMinutes * 30 * 24;
-        System.out.println("За 1 день машина произвела " + bottleOneDay + " штук бутылок");
+        System.out.println("За 1 день машина произвела " + bottleOneDay + " бутылок");
 
         int bottleThreeDay = bottleOneDay * 3;
-        System.out.println("За 3 дня машина произвела " + bottleThreeDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + bottleThreeDay + " бутылок");
 
         int bottleOneMonth = bottleOneDay * 30;
-        System.out.println("За 1 месяц (30 дней) машина произвела " + bottleOneMonth + " штук бутылок");
+        System.out.println("За 1 месяц (30 дней) машина произвела " + bottleOneMonth + " бутылок");
 
     }
 }
