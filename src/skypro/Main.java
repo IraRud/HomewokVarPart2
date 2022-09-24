e6dfa6753ea38507f73c904d385d96cb347c708e
rudskaya irina <irarudskaya5@gmail.com>
2022-09-24 14:37:07 +0300

    Находим количество листов на одного

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -3,13 +3,18 @@ package skypro;
 public class Main {
     public static void main(String[] args) {
 
-        byte studentsLP = 23;
-        byte studentsAS = 27;
-        byte studentsEA = 30;
-        short totalPaper = 480;
+        byte bottleTwoMinutes = 16;
+        int bottleTwentyMinutes = bottleTwoMinutes * 10;
+        System.out.println("За 20 минут машина произвела " + bottleTwentyMinutes + " штук бутылок");
 
-        float paperForOne = totalPaper / (studentsAS + studentsLP + studentsEA);
-        System.out.println("Количество листов бумаги на одного ученика " + paperForOne);
+        int bottleOneDay = bottleTwoMinutes * 30 * 24;
+        System.out.println("За 1 день машина произвела " + bottleOneDay + " штук бутылок");
+
+        int bottleThreeDay = bottleOneDay * 3;
+        System.out.println("За 3 дня машина произвела " + bottleThreeDay + " штук бутылок");
+
+        int bottleOneMonth = bottleOneDay * 30;
+        System.out.println("За 1 месяц (30 дней) машина произвела " + bottleOneMonth + " штук бутылок");
 
     }
 }
