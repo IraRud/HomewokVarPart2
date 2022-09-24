1a375942c9b90aac58636ef12db7bc7ef4439a3b
rudskaya irina <irarudskaya5@gmail.com>
2022-09-24 15:34:38 +0300

    Task 6. Рассчет весп

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -9,7 +9,15 @@ public class Main {
         int numberMilk = 2;
         int weightMilk = 105;
         int totalWeightMilk = numberMilk * weightMilk;
-
+        int numberIceCream = 2;
+        int weightIceCream = 100;
+        int totalWeightIceCream = numberIceCream * weightIceCream;
+        int numberEggs = 4;
+        int weightEggs = 70;
+        int totalWeightEggs = numberEggs * weightEggs;
+        float totalWeightBreakfast = (totalWeightEggs + totalWeightBananas + totalWeightIceCream
+                + totalWeightMilk) / 1000f;
+        System.out.println("Общий вес завтрака " + totalWeightBreakfast + " кг.");
 
 
     }
