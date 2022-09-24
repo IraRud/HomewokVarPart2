4d3f3f55f1fa50faadc9b7f46d93405c6399ce52
rudskaya irina <irarudskaya5@gmail.com>
2022-09-24 13:51:16 +0300

    Находим количество листов на одного

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -3,31 +3,13 @@ package skypro;
 public class Main {
     public static void main(String[] args) {
 
-        float variableFloat = 27.12f;
-        long variableLong = 987_678_965_549L;
-        byte variableByteOne = 2;
-        short variableShortOne = 786;
-        boolean variableBoolean = false;
-        short variableShortTwo = 569;
-        short variableShortThree = -159;
-        int variableInt = 27897;
-        byte variableByteTwo = 67;
+        byte studentsLP = 23;
+        byte studentsAS = 27;
+        byte studentsEA = 30;
+        short totalPaper = 480;
 
-        double variableDouble = 13.256;
-        char variableChar = '?';
-
-        System.out.println("\nvariableFloat = " + variableFloat);
-        System.out.println("variableLong = " + variableLong);
-        System.out.println("variableByteOne = " + variableByteOne);
-        System.out.println("variableShortOne = " + variableShortOne);
-        System.out.println("variableBoolean = " + variableBoolean);
-        System.out.println("variableShortTwo = " + variableShortTwo);
-        System.out.println("variableShortThree = " + variableShortThree);
-        System.out.println("variableInt = " + variableInt);
-        System.out.println("variableByteTwo = " + variableByteTwo);
-        System.out.println("\nИнициализируем новые переменные с недостоющими типами.");
-        System.out.println("variableDouble = " + variableDouble);
-        System.out.println("variableChar = " + variableChar);
+        float paperForOne = totalPaper / (studentsAS + studentsLP + studentsEA);
+        System.out.println("Количество листов бумаги на одного ученика " + paperForOne);
 
     }
 }
